2b7ff7232125e14065002df8b78e7acec0b37b67
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2020-04-20 13:43:51 +0900

    create class compnent and functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 // JSXを使用する際は、Reactをインポートする必要がある
-import React, { Component } from "react";
+import React from "react";
 
 // JSX表記方法
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onChange={() => {
-            console.log("I am clicked!!");
-          }}
-        ></input>
-      </React.Fragment>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input
+//           type="text"
+//           onChange={() => {
+//             console.log("I am clicked!!");
+//           }}
+//         ></input>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
